45cacc110f5c29d38fdc5fdef20d86f47d4cb298
HemanthKumar4444 <punnahemanthkumar4@gmail.com>
2026-05-12 12:39:27 +0530

    Added service and BookServiceImpl

---
+++ Libary-backend/src/main/java/com/example/Libary_backend/service/impl/BookServiceImpl.java
@@ -0,0 +1,56 @@
+package com.example.Libary_backend.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.Libary_backend.dto.BookRequestDTO;
+import com.example.Libary_backend.dto.BookResponseDTO;
+import com.example.Libary_backend.entity.Book;
+import com.example.Libary_backend.mapper.BookMapper;
+import com.example.Libary_backend.repository.BookRepository;
+import com.example.Libary_backend.service.BookService;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
+
+    public BookServiceImpl(BookRepository bookRepository, BookMapper bookMapper) {
+        this.bookRepository = bookRepository;
+        this.bookMapper = bookMapper;
+    }
+
+    @Override
+    public BookResponseDTO addBook(BookRequestDTO requestDTO) {
+        Book book = bookMapper.toEntity(requestDTO);
+        Book savedBook = bookRepository.save(book);
+        return bookMapper.toResponseDTO(savedBook);
+    }
+
+    @Override
+    public List<BookResponseDTO> getAllBooks() {
+        return bookRepository.findAll()
+                .stream()
+                .map(bookMapper::toResponseDTO)
+                .toList();
+    }
+
+    @Override
+    public List<BookResponseDTO> getAvailableBooks() {
+        return bookRepository.findByAvailabilityTrue()
+                .stream()
+                .map(bookMapper::toResponseDTO)
+                .toList();
+    }
+
+    @Override
+    public List<BookResponseDTO> searchBooks(String keyword) {
+        return bookRepository
+                .findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(keyword, keyword)
+                .stream()
+                .map(bookMapper::toResponseDTO)
+                .toList();
+    }
+}
