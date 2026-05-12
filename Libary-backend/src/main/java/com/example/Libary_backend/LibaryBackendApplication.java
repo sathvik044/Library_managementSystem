a56b0554554414fa7f764649a86249b7d31aea2b
sathvik044 <sathvikvadapalli123@gmail.com>
2026-05-12 11:50:53 +0530

    initial commit

---
+++ Libary-backend/src/main/java/com/example/Libary_backend/LibaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Libary_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibaryBackendApplication.class, args);
+	}
+
+}
